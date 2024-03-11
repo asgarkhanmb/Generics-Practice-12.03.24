df5c83832478f5ae04f15ba0d3e7d21a4d91009a
unknown <asgarkhanmb@code.edu.az>
2024-03-12 01:20:39 +0400

    Generics Practice

---
+++ Generics/Program.cs
@@ -0,0 +1,94 @@
+﻿//using Generics;
+//using System.Xml.Serialization;
+
+//IntList datas = new IntList();
+
+//datas.Add(1);
+//datas.Add(2);
+//datas.Add(3);
+//datas.Add(4);
+//datas.Add(5);
+
+//int[] result=datas.GetAll();
+
+//foreach(int item in result)
+//{
+//    Console.WriteLine(item); 
+//}
+
+//StringList stringList = new StringList();
+
+//stringList.Add("salam");
+//stringList.Add("Sagol");
+
+//string[] result2=stringList.GetAll();
+
+//foreach(string item in result2)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//using Generics;
+
+//DataList<int> datas = new DataList<int>();
+
+//datas.Add(100);
+//datas.Add(200);
+//datas.Add(300);
+
+//var result=datas.GetAll();
+
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);    
+//}
+
+//DataList<string> dataList = new DataList<string>();
+
+//dataList.Add("salam");
+//dataList.Add("salam2");
+
+//string[] result2 = dataList.GetAll();
+
+//foreach (string item in result2)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+//DataList<bool> dataList1 = new DataList<bool>();
+
+//dataList1.Add(true);
+//dataList1.Add(false);
+//dataList1.Add(true);
+
+//var result3 = dataList1.GetAll();   
+
+//foreach (bool item in result3)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
+//using Generics;
+
+//DataList<Book> books = new DataList<Book>();
+
+//books.Add(new Book { Id = 1, Name = "test1" });
+//books.Add(new Book { Id = 2, Name = "test2" });
+//books.Add(new Book { Id = 3, Name = "test3" });
+
+//var result=books.GetAll();
+
+//foreach(var item in result)
+//{
+//    Console.WriteLine(item.Name);
+//}
+
+
+
+
